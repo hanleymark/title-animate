d82d86971c508b98c6e6f9882952cb1663114431
hanleymark <19717194-hanleymark@users.noreply.replit.com>
2023-05-08 11:02:51 +0000

    set up cursor animation timing

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const styleSheet =
-  document.querySelector("#extra-styles");
+const styleSheet = document.querySelector("#extra-styles");
+const titleContainer = document.querySelector(".title-container");
+const title = "MARK HANLEY";
+const root = document.querySelector(":root");
+root.style.setProperty('--number-of-letters', `${title.length}`);
+
+addTitleToPage(title, titleContainer);
 
 const letters = Array.from(document.querySelectorAll(".letter"));
 
 let rule = "";
 letters.map((l, i) => {
-  rule += `.letter:nth-child(${i+1})`;
+  rule += `.letter:nth-child(${i + 1})`;
   rule += "{";
   rule += "animation: rise 0.5s forwards ease-in-out;";
   rule += `animation-delay: calc(var(--tick) * ${i + 1});`;
   rule += "}";
 });
+
+// Make html element visible (prevented FOAC)
 styleSheet.innerHTML = rule;
+
+function addTitleToPage(title, element) {
+  for (letter of title) {
+    const span = document.createElement("span");
+    span.classList.add("letter");
+    span.innerText = (letter === " ") ? "_" : letter;
+    element.appendChild(span);
+  }
+  const underline = document.createElement("span");
+  //underline.classList.add("letter");
+  underline.classList.add("underline");
+  underline.innerText = "_";
+  element.appendChild(underline);
+}
